f3a7ddca72b554d5e31d5faf3b0b33c4a845a3d2
arnaud-23 <arnaud.h.lefevre@gmail.com>
2016-10-17 13:22:35 +0200

    install bootstrap

---
+++ src/AppBundle/DependencyInjection/AppExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace AppBundle\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+/**
+ * @author Arnaud Lefevre <arnaud.h.lefevre@gmail.com>
+ */
+class AppExtension extends Extension
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.xml');
+    }
+}
